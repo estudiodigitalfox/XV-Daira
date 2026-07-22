f8cd6179064d10d34d754a87ba65d58236fe6b75
estudiodigitalfox <estudiodigitalfox@gmail.com>
2026-07-21 19:36:18 -0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,48 @@
+const CONFIG = {
+  // ==========================
+  // DATOS DEL EVENTO
+  // ==========================
+
+  nombreEvento: "XV Daira Sayuri",
+
+  fechaLimiteTexto: "Fecha límite por definir",
+  fechaLimiteSistema: "2026-12-20T23:59:59",
+
+  // ==========================
+  // DISEÑO
+  // ==========================
+
+  colorPrincipal: "#7A4FA3",
+  colorSecundario: "#F5EEFA",
+  colorDetalle: "#D4AF37",
+
+  animacion: "estrellas",
+  usarColorPrincipalEnAnimacion: true,
+
+  // ==========================
+  // CONFIRMACIÓN PERSONALIZADA
+  // ==========================
+
+  solicitarCodigo: true,
+  permitirModificarConfirmacion: false,
+
+  // ==========================
+  // PASE DIGITAL
+  // ==========================
+
+  qrHabilitado: true,
+
+  // true = muestra QR y permite guardar el pase
+  // false = confirma familia, mesa y asistentes sin mostrar QR
+
+  // ==========================
+  // CONEXIÓN CON GOOGLE
+  // ==========================
+
+  mesaHabilitada: true,
+
+// true = muestra la mesa asignada
+// false = oculta por completo la mesa
+
+  scriptURL: "https://script.google.com/macros/s/AKfycbzxgbaVReMCdfd5mc1a1iylEZMdYuJekPJyubi0qGOXJEDvCuF_5da9u6GHdaIImZFBhw/exec"
+};
